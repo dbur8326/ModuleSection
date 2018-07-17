4d636a67347c3ba3b6fd13078f18d6b89c1fbc5a
Jared Grant <jared.grant@canyonsdistrict.org>
2018-07-17 11:39:45 -0600

    Inital Commit

---
+++ src/bootstrap.js
@@ -0,0 +1,5 @@
+import navigation, { greeting, multiply} from './navigation';
+
+console.log(navigation());
+console.log(navigation.multiply(5, 9));
+console.log(navigation.greeting);
---
+++ src/navigation.js
@@ -0,0 +1,9 @@
+export default function () {
+    return '<div>Logo</div>';
+};
+
+export function multiply(num1, num2){
+    return num1 * num2;
+};
+
+export const greeting='Hello world';
